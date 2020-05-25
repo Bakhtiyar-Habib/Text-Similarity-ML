181979a4dc505f0fa00a932e09fd9bf2cf08b3e3
Bakhtiyar Habib <bakhtiyar.habib19@gmail.com>
2020-05-25 21:21:00 +0600

    created basic django main app and home view template

---
+++ src/main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+	print(args, kwargs)
+	print(request.user)
+	return render(request, "home.html", {})
